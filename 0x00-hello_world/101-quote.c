50c909cd63e79d694cce0c1543044c64f53c73c5
Jmuinde <jmuindedt@gmail.com>
2023-06-15 13:50:50 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <unistd.h>
 #include <string.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, strlen(message));
-    return 1;
+    return (0);
 }
-
