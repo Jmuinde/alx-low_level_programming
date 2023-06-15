e277f0015f4da99cffde97a5570e3426381e0da7
Jmuinde <jmuindedt@gmail.com>
2023-06-15 13:43:43 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+int main() {
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, strlen(message));
+    return 1;
 }
+
