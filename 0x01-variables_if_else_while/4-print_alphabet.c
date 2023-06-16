692774e5127e468dbf8c401541c0b0a9c0be58d1
Jmuinde <jmuindedt@gmail.com>
2023-06-16 13:52:34 -0700

    ecxept q and x

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alph;
+	
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph !='q' && alph != 'e')
+			putchar(alph);
+        }
+        putchar('\n');
+        return (0);
+}
