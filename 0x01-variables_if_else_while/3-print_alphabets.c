25a9eb5515f53f8fe9f28b759ded447d1b5793ff
Jmuinde <jmuindedt@gmail.com>
2023-06-16 13:32:47 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int alph;
+	int Alph;
+
+        for (alph = 'a'; alph <= 'z'; alph++)
+        {
+                putchar(alph);
+        }
+	for (Alph = 'A'; Alph <= 'Z'; Alph++)
+	{
+		putchar(Alph);
+	}
+	putchar('\n');
+        return (0);
+}
