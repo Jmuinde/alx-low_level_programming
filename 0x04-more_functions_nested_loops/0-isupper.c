e0c50e7f3c312c9e3ee2eae7df6ca4736aa70654
Jmuinde <jmuindedt@gmail.com>
2023-06-22 03:59:50 -0700

    checking the type of charCTER

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase
- *@c - the character to be checked
- *
- *Return: 1 if the character is uppercase and 0 if it is anything else
- */
-int _isupper(int c)
+  * _isupper - Check if a letter is upper
+  * @x: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
+int _isupper(int x)
 {
-	if (c >= 65 && c <= 90)
+	if (x >= 65 && x <= 90)
 	{
 		return (1);
 	}
-	
+
 	return (0);
 }
