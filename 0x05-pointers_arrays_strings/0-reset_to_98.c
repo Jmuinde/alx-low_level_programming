ca615edd401eeaac2da77ff0222664e983af6c47
Jmuinde <jmuindedt@gmail.com>
2023-06-26 05:13:44 -0700

    pointer to change value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h" 
+
+/**
+ *reset_to_98 - function to update int parameter
+ *@n: pointer to n
+ * Return:if the function is sucess and - if otherwise 
+ */
+void reset_to_98(int *n);
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
