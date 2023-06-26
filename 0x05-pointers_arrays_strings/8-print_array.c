30f174d9a22c769529f26d342745f94263ec80ec
Jmuinde <jmuindedt@gmail.com>
2023-06-26 09:07:50 -0700

    priniting n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,25 +4,23 @@
 /**
  * print_array - function to print n elemnts of an array of integer
  *
- * @a: Pointer variable 
+ * @a: Pointer variable
  * @n: number of integres in the array
- * Return: void 
+ * Return: void
  */
 void print_array(int *a, int n)
-{	
-	int i; 
-
+{
+	int i;
+	
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 	
 		if (i != (n - 1))
 		{
-	
 			printf(", ");
 
-		}	
+		}
 	}
-
 	printf("\n");
-}	
+}
