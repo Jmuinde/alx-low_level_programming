72589df76270511505aee89939143d51ca8b85cd
Jmuinde <jmuindedt@gmail.com>
2023-06-26 05:46:31 -0700

    swapping

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,10 @@
 /**
  * swap_int - function to swap two numbers
  *
- * @a - integer to be swiped
- * @b - integer to be swiped
+ * @a: integer to be swiped
+ * @b: integer to be swiped
  * Return: 1 on succes and -1 otherwise
  */
-
 void swap_int(int *a, int *b);
 void swap_int(int *a, int *b)
 {
