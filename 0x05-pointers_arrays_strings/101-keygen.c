4f075269998abe99ddc3f435714b86f9296d72b6
Jmuinde <jmuindedt@gmail.com>
2023-06-27 03:04:11 -0700

    generating a random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * randompassword - function to generate the password 
+ * @n: length of the password 
+ * Return: void 
+ */
+void randompassword(int n)
+ {
+	char charstopickfrom[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+	char password[n + 1];
+	int i;
+	
+	srand(time(0));
+	for (i = 0; i < n; i++)
+	{
+		int randomindex = rand() % (sizeof(charstopickfrom) - 1);
+		password[i] = charstopickfrom[randomindex];
+	}
+ 
+ 	password[n] = '\0';
+ 	printf("congrats here is your random password: %s\n", password);
+ }
---
+++ 0x05-pointers_arrays_strings/101-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 10;
+ 	randompassword(n);
+ 	return 0;
+}
