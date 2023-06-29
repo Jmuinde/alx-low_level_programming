0e33f1caa69ce0b3b10d427479346fb49acae31d
Jmuinde <jmuindedt@gmail.com>
2023-06-29 01:24:00 -0700

    concntinating strings n times

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ * *_strncat - The concantinating function
+ * @*dest: string to be concantinated
+ * @*src: string to be concantinated
+ * @n: number of times 
+ * return: resulting string 
+ */
+char *_strncat(char *dest, char *src, int n)
+{	
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
+
