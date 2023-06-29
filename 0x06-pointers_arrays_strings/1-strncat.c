3d55075fa271876c08dd8638e2b33aea8ccf4bdf
Jmuinde <jmuindedt@gmail.com>
2023-06-29 02:45:29 -0700

    concant

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,25 @@
-# include "main.h"
+#include "main.h"
 
 /**
- * *_strncat - The concantinating function
- * @dest: string to be concantinated
- * @src: string to be concantinated
- * @n: number of times
- * return: resulting string
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
  */
-char *_strncat(char *dest, char *src, int n)
-{	
-	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++);
-	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0'; c++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[c];
+		i++;
 	}
+
+	dest[i] = '\0';
 	return (dest);
 }
