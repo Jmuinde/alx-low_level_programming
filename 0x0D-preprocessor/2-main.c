96a78a194b133a917cbfaef2b013e6c138bc67df
Jmuinde <jmuindedt@gmail.com>
2023-07-17 05:51:36 -0700

    program to print the file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 
 /**
  * main - the function to be executed
- * Return: 0 on success
+ * Return: 0 on success, 1 on failure
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
