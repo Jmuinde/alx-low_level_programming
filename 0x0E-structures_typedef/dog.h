25fe005d4408ad18ef9521a1a5435321cf8bc9d4
Jmuinde <jmuindedt@gmail.com>
2023-07-18 13:59:23 -0700

    function to initialize datatype of type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,11 @@ typedef struct dog
 	char *owner;
 } dog;
 #endif
+#ifndef _Prototypes_H
+#define _Prototypes_H
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
