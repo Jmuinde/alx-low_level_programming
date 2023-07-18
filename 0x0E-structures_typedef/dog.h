812292c3074187245c4959813366871467d68134
Jmuinde <jmuindedt@gmail.com>
2023-07-18 13:26:51 -0700

    defyning a new type structer

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCTERS
+#define STRUCTERS
+/**
+ * struct dog - description for the  pet
+ * @name: name of the dog
+ * @age: age of the pet
+ * @owner: name of the owner
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
