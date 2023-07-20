e9c94cd85d075921ffade2f6c6d720638265011d
Jmuinde <jmuindedt@gmail.com>
2023-07-20 06:35:17 -0700

    a variadic fucntion that returns the sum of all it arguments

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,2 +1,20 @@
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdarg.h>
+/**
+ * struct vab - datatype of struct
+ *
+ * @arg: member of the datatype
+ * @pf: pointer function
+ */
+typedef struct vab
+{
+	char *arg;
+	void (*pf)(va_list);
+} vab;
+#endif
+#ifndef _PROTOTYPE_H
+#define _PROTOTYPE_H
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+#endif
