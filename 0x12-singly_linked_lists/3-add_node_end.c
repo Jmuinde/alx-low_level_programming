453b8e3dc6ac36bb3117ba016ca7fdc39554f181
Jmuinde <jmuindedt@gmail.com>
2023-07-28 01:47:40 -0700

    addinjg a new node to existing data structer

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	int a;
-	int n = 0;
+	int x = 0;
 	list_t *new;
 	list_t *temp;
 
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	for (a = 0; str[a] != '\0'; a++)
-		n++;
+		x++;
 
 	new->len = a;
 	new->str = strdup(str);
