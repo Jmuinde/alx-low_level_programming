2a05867c2b64083667aa0f7bda498d942267fa58
Jmuinde <jmuindedt@gmail.com>
2023-07-28 02:47:36 -0700

    function to free linked list memeory

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * free_list - a function that frees a list_t list.
- * @head: struct
+ * free_list - function to free the memory of a linked list
+ * @head: head poineter
  * Return: free
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
+	if (head == 0)
 		return;
 
-	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
+	if ((*head).next != 0)
+		free_list((*head).next);
+	free((*head).str);
 	free(head);
 }
