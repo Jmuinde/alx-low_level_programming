56d88da6034044e093b6560f16be55a07ee0c4de
Jmuinde <jmuindedt@gmail.com>
2023-08-01 07:05:18 -0700

    freeing allocated memory of a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function to free allocated memory of a list
+ * 
+ * @head: head pointer
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head)
+	{
+		while (*head != NULL)
+		{
+			ptr = *head;
+			*head = (*ptr).next;
+			free(ptr);
+		}
+	}
+
+}
