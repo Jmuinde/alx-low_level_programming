8a8a1159444709718a09c09559939da694a9fc18
Jmuinde <jmuindedt@gmail.com>
2023-08-01 07:41:25 -0700

    getting the sum of nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum number of
+ * total nodes in a linked list
+ * @head: head pointer
+ * Return: total
+ */
+int sum_listint(listint_t *head)
+{
+	int summ = 0;
+
+	if (head ==0)
+		return (0);
+
+	while (head != 0)
+	{
+		summ =+ (*head).n;
+		head = (*head).next;
+	}
+
+	return (summ);
+}
