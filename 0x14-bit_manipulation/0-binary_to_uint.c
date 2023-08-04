c4ddf87e005d58a9b5e2dcb6a94e1ee5fbb347a3
Jmuinde <jmuindedt@gmail.com>
2023-08-04 01:09:35 -0700

    function to convert binary to unisigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function to convert binary number
+ * to unsinged integer
+ * @b: constant pinter variable 
+ * Return: converted integer or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int pow = 0;
+	int pos = 1;
+	int k = 0;
+
+	if (b == 0)
+		return (0);
+
+	while (b[k + 1])
+	{
+		if (b[k] != '0' && b[k] != '1')
+			return (0);
+		k++;
+	}
+
+	while (k >= 0)
+	{
+		pow += ((b[k] - '0') * pos);
+		pos *= 2;
+		k--;
+	}
+
+
+	return (pow);
+
+}
